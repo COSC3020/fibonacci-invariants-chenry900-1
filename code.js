7f7296a7d88477db2fc4d2e0ff09958b1e816afa
chenry900 <158115154+chenry900@users.noreply.github.com>
2025-02-05 15:32:40 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,9 @@
 function fib(n) {
-    return n;
+        if (n == 0)
+        return [0];
+    if (n == 1)
+        return [0,1];
+    var array = fib(n-1);
+    array.push(array[n - 2]+array[n - 1]);
+    return array;
 }
